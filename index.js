5b3992ce187f73e59def2727939c046dbec75234
Harshil173 <harshilpatadiya8@gmail.com>
2025-07-31 19:06:27 +0530

    Created Custom React App

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-{/* <div id="parent">
-        <div id="child1">
-            <h1>I am H1 Tag</h1>
-            <h2>I am H2 Tag</h2>
-        </div>
-        <div id="child2">
-            <h1>I am H1 Tag</h1>
-            <h2>I am H2 Tag</h2>
-        </div>
-    <div> */}
-
+import React from "react"
+import ReactDOM from "react-dom/client"
 const parent = 
     React.createElement("div", {id : "parent"},
     [React.createElement("div", {id : "child"},
     [React.createElement("h1",{},"I am an H1 Tag"),
     React.createElement("h2",{},"I am an H2 Tag"),     
     ]),
-    React.createElement("div", {id : "child"},
+    React.createElement("div", {id : "child2"},
     [React.createElement("h1",{},"I am an H1 Tag"),
     React.createElement("h2",{},"I am an H2 Tag"),     
     ]),]
 )
 const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(parent)
+// console.log(parent)
 root.render(parent)
